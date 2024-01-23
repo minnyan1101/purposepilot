45d338ae5cf0b415e7710912260fd3e273523b19
kcsoita.20210178 <kcsoita.20210178@gmail.com>
2024-01-23 16:44:29 +0900

    w

--- frontend/src/components/ReviewQuestionSlider.vue
+++ frontend/src/components/ReviewQuestionSlider.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Slider from './Slider.vue';
 const model = defineModel()
+const props = defineProps(["label"])
 </script>
 <template>
   <div class="grid grid-cols-[auto_1fr_auto] grid-rows-[auto_auto] gap-2">
-      <p class="col-start-1 col-end-4 row-start-1 row-end-2">Question 1</p>
+      <p class="col-start-1 col-end-4 row-start-1 row-end-2">{{ props.label }}</p>
       <p class="col-start-1 col-end-2 row-start-2 row-end-3">悪い</p>
       <Slider max="1" min="-1" step="0.01" v-model="model"></Slider>
       <p class="col-start-3 col-end-4 row-start-2 row-end-3">良い</p>
--- frontend/src/components/pages/ReviewCreatePage.vue
+++ frontend/src/components/pages/ReviewCreatePage.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import ReviewQuestionSlider from '../ReviewQuestionSlider.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const target_purpose_id = computed(() => route.params.id)
 
 const questions = ref({
   q1: 0,
@@ -11,12 +15,14 @@ const questions = ref({
 </script>
 
 <template>
-  <div class="flex flex-col w-full max-w-xl py-4">
-    <div>
+  <div class="flex flex-col w-full max-w-xl py-4 gap-4">
+    <div  class="self-start">
       <RouterLink to="/reviews">
         <div class="border-2 rounded-md w-fit py-1 px-2 border-neutral-500 hover:border-neutral-700 bg-neutral-50 hover:bg-neutral-300">Back</div>
       </RouterLink>
     </div>
-    <ReviewQuestionSlider v-model="questions.q1" />
+    <ReviewQuestionSlider v-model="questions.q1" label="Q1"/>
+    <ReviewQuestionSlider v-model="questions.q2" label="Q2"/>
+    <ReviewQuestionSlider v-model="questions.q3" label="Q3"/>
   </div>
 </template>
